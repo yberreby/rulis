f687496a4ae39e07c099ec96e75c681fde83807f
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-22 20:59:44 +0200

    write LALRPOP grammar
    Write the grammar, a simple unit test and ignore generated grammar file.

--- parser/src/ast.rs
+++ parser/src/ast.rs
@@ -1,6 +1,20 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Operator {
     Plus,
     Minus,
     Multiply,
     Divide,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Integer(i64),
+    Operation(Operation),
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Operation {
+    pub operator: Operator,
+    pub operands: Vec<Expr>,
 }
