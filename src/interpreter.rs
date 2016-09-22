686fc1556090a2dad60b3d3d77db22019b44ad9e
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-22 22:08:09 +0200

    get rid of boxed closure in eval_op

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -11,30 +11,43 @@ pub fn eval_expr(expr: &Expr) -> i64 {
 fn eval_op(op: &Operation) -> i64 {
     let mut iter = op.operands.iter();
     let initial_accumulator: i64;
-    // Yes this is horribly inefficient.
-    let f: Box<Fn(i64, i64) -> i64>;
-
+    let f: fn(i64, i64) -> i64;
 
     match op.operator {
         Operator::Plus => {
             initial_accumulator = 0;
-            f = Box::new(|a, b| a + b)
+            fn g(a: i64, b: i64) -> i64 {
+                a + b
+            }
+            f = g;
         }
         Operator::Minus => {
             let val = eval_expr(iter.next().unwrap());
             initial_accumulator = if op.operands.len() == 1 { -val } else { val };
-            f = Box::new(|a, b| a - b)
+
+            fn g(a: i64, b: i64) -> i64 {
+                a - b
+            }
+            f = g;
         }
         Operator::Multiply => {
             initial_accumulator = 1;
-            f = Box::new(|a, b| a * b)
+
+            fn g(a: i64, b: i64) -> i64 {
+                a * b
+            }
+            f = g;
         }
         Operator::Divide => {
             initial_accumulator = eval_expr(iter.next().unwrap());
-            f = Box::new(|a, b| a / b)
+
+            fn g(a: i64, b: i64) -> i64 {
+                a / b
+            }
+            f = g;
         }
     }
 
     iter.fold(initial_accumulator,
-              |acc, operand| (*f)(acc, eval_expr(operand)))
+              |acc, operand| f(acc, eval_expr(operand)))
 }
