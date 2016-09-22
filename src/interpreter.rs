9acaed6c9f01606c6195c8569f2c045106cc17a2
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-22 21:51:22 +0200

    get multiplication and integer division working

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -30,53 +30,24 @@ fn eval_op(op: &Operation) -> i64 {
 
             acc
         }
-        _ => unimplemented!(),
+        Operator::Multiply => {
+            let mut acc = 1;
+
+            for operand in &op.operands {
+                acc *= eval_expr(operand)
+            }
+
+            acc
+        }
+        Operator::Divide => {
+            let mut iter = op.operands.iter();
+            let mut acc = eval_expr(&iter.next().unwrap());
+
+            for operand in iter {
+                acc /= eval_expr(operand)
+            }
+
+            acc
+        }
     }
 }
-
-// #[cfg(test)]
-// mod test {
-//     use super::eval_op;
-//     use ast::{Expr, Operation, Operator};
-//
-//     #[test]
-//     fn eval_op_works() {
-//         let runs = [(Operation {
-//                         operator: Operator::Plus,
-//                         operands: vec![
-//                         Expr::Integer(5),
-//                         Expr::Integer(1),
-//                         Expr::Integer(27),
-//                     ],
-//                     },
-//                      33),
-//                     (Operation {
-//                         operator: Operator::Minus,
-//                         operands: vec![
-//                         Expr::Integer(247),
-//                         Expr::Integer(18),
-//                         Expr::Integer(297),
-//                     ],
-//                     },
-//                      -68),
-//                     (Operation {
-//                         operator: Operator::Multiply,
-//                         operands: vec![
-//                         Expr::Integer(247),
-//                         Expr::Integer(18),
-//                         Expr::Integer(297),
-//                     ],
-//                     },
-//                      1320462)];
-//
-//         for &(ref operation, expected) in &runs {
-//             let actual = eval_op(&operation);
-//             assert_eq!(actual,
-//                        expected,
-//                        "expected {}, got {} when evaluating {:?}",
-//                        expected,
-//                        actual,
-//                        operation);
-//         }
-//     }
-// }
--- src/test.rs
+++ src/test.rs
@@ -28,9 +28,18 @@ fn invalid_code_is_rejected() {
 
 #[test]
 fn evaluation_works() {
-    let runs = [("(+ 42 268)", 310), ("(- 96)", -96), ("(- 96 20 1 1 1)", 73)];
+    let runs = [("(+ 42 268)", 310),
+                ("(- 96)", -96),
+                ("(- 96 20 1 1 1)", 73),
+                ("(* 5 7)", 35),
+                ("(* 2 2 2)", 8),
+                ("(* 5 7 11)", 385),
+                ("(* 1 1 5 1 1 1 7 1 1 1 11)", 385),
+                ("(* 1 1 5 1 1 1 7 0 1 1 11)", 0),
+                ("(/ 128674 48 3)", 893)];
 
     for &(ref operation, expected) in &runs {
+        print!("{}... ", operation);
         let actual = eval(&operation);
         assert_eq!(actual,
                    expected,
@@ -38,6 +47,6 @@ fn evaluation_works() {
                    expected,
                    actual,
                    operation);
-        println!("{}: OK", operation);
+        println!("OK");
     }
 }
