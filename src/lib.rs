4185266d72e60fa5d50f2fb21ba4be78d0341276
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-24 17:06:58 +0200

    add basic error handling
    Handle ^D (EOF), I/O errors, parse errors

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ mod grammar;
 mod ast;
 mod interpreter;
 
-pub fn eval(s: &str) -> i64 {
-    let expr = grammar::parse_Expr(s).expect("failed to parse");
-    interpreter::eval_expr(&expr)
+pub fn eval(s: &str) -> Result<i64, String> {
+    let expr = try!(grammar::parse_Expr(s).map_err(|e| format!("{:?}", e)));
+    Ok(interpreter::eval_expr(&expr))
 }
 
 #[cfg(test)]
