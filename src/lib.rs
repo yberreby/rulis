35236355da00c9ae2bdd3164b5c78e78167b2ee5
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-24 17:21:07 +0200

    add boilerplate for evaluation error handling

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,8 @@ mod interpreter;
 
 pub fn eval(s: &str) -> Result<i64, String> {
     let expr = try!(grammar::parse_Expr(s).map_err(|e| format!("{:?}", e)));
-    Ok(interpreter::eval_expr(&expr))
+    let res = try!(interpreter::eval_expr(&expr));
+    Ok(res)
 }
 
 #[cfg(test)]
