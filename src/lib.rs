5b836806dc5c2738c9be9e1e5503d1f1f2b6568d
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-25 14:12:07 +0200

    fix ast debug formatting

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod interpreter;
 
 pub fn eval(s: &str) -> Result<ast::Expr, String> {
     let mut sexpr = try!(parser::parse(s).map_err(|e| format!("{:?}", e)));
-    println!("AST\n: {:?}", sexpr);
+    println!("AST: {:?}", sexpr);
     println!("S-Expression: {}", sexpr);
     let res = try!(interpreter::eval_sexpr(&mut sexpr));
     Ok(res)
