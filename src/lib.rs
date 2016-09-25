5de03ecb92ddbf8847b11ed57a731fc55eb0d93a
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-25 15:36:48 +0200

    fix error display

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod ast;
 mod interpreter;
 
 pub fn eval(s: &str) -> Result<ast::Expr, String> {
-    let mut sexpr = try!(parser::parse(s).map_err(|e| format!("{:?}", e)));
+    let mut sexpr = try!(parser::parse(s).map_err(|e| format!("{}", e)));
     debug!("S-Expression (debug): {:?}", sexpr);
     debug!("S-Expression (display): {}", sexpr);
     let res = try!(interpreter::eval_sexpr(&mut sexpr));
