200aa524532e9d53bbbf601670fda8d9fdfd05a6
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-09-26 12:40:53 +0200

    use test_runs in qexpressions_builtins_work

--- src/test.rs
+++ src/test.rs
@@ -2,12 +2,7 @@ use value::Expr;
 use eval;
 use std::fmt::Debug;
 
-#[test]
-fn invalid_code_is_rejected() {
-    let src = "(+ 5 ((* 2 428))";
-    assert!(eval(src).is_err());
-}
-
+/// Generic helper method to simplify testing.
 fn test_runs<'a,
              T: PartialEq + Debug,
              U,
@@ -33,41 +28,38 @@ fn test_runs<'a,
 
 
 #[test]
-fn simple_arithmetic_evaluation_works() {
-    let runs = [("(+ 42 268)", 310),
-                ("(- 96)", -96),
-                ("(- 96 20 1 1 1)", 73),
-                ("(* 5 7)", 35),
-                ("(* 2 2 2)", 8),
-                ("(* 5 7 11)", 385),
-                ("(* 1 1 5 1 1 1 7 1 1 1 11)", 385),
-                ("(* 1 1 5 1 1 1 7 0 1 1 11)", 0),
-                ("(/ 128674 48 3)", 893)];
+fn invalid_code_is_rejected() {
+    let src = "(+ 5 ((* 2 428))";
+    assert!(eval(src).is_err());
+}
 
-    test_runs(runs.to_vec().into_iter(), |s| s, |i| Ok(Expr::Integer(i)));
+#[test]
+fn simple_arithmetic_evaluation_works() {
+    let runs = vec![("(+ 42 268)", 310),
+                    ("(- 96)", -96),
+                    ("(- 96 20 1 1 1)", 73),
+                    ("(* 5 7)", 35),
+                    ("(* 2 2 2)", 8),
+                    ("(* 5 7 11)", 385),
+                    ("(* 1 1 5 1 1 1 7 1 1 1 11)", 385),
+                    ("(* 1 1 5 1 1 1 7 0 1 1 11)", 0),
+                    ("(/ 128674 48 3)", 893)];
+
+    test_runs(runs.into_iter(), |s| s, |i| Ok(Expr::Integer(i)));
 }
 
 #[test]
 fn qexpressions_builtins_work() {
-    let runs = [("(list 1 2 3 4)", "{1 2 3 4}"),
-                ("{head (list 1 2 3 4)}", "{head (list 1 2 3 4)}"),
-                ("(head (list 1 2 3 4))", "{1}"),
-                ("(eval {head (list 1 2 3 4)})", "{1}"),
-                ("(tail {tail tail tail})", "{tail tail}"),
-                ("(eval (tail {tail tail {5 6 7}}))", "{6 7}"),
-                ("(eval {+ 1 2})", "3"),
-                ("(join {+ 1 2} {6548} {/ / /})", "{+ 1 2 6548 / / /}")];
+    let runs = vec![("(list 1 2 3 4)", "{1 2 3 4}"),
+                    ("{head (list 1 2 3 4)}", "{head (list 1 2 3 4)}"),
+                    ("(head (list 1 2 3 4))", "{1}"),
+                    ("(eval {head (list 1 2 3 4)})", "{1}"),
+                    ("(tail {tail tail tail})", "{tail tail}"),
+                    ("(eval (tail {tail tail {5 6 7}}))", "{6 7}"),
+                    ("(eval {+ 1 2})", "3"),
+                    ("(join {+ 1 2} {6548} {/ / /})", "{+ 1 2 6548 / / /}")];
 
-    for &(ref operation, expected) in &runs {
-        print!("{}... ", operation);
-        let expected = Ok(expected);
-        let actual = eval(&operation).map(|x| x.to_string());
-        assert_eq!(actual.as_ref().map(|s| &s[..]),
-                   expected,
-                   "expected {:?}, got {:?} when evaluating {:?}",
-                   expected,
-                   actual,
-                   operation);
-        println!("OK");
-    }
+    test_runs(runs.into_iter(),
+              |r| r.map(|x| x.to_string()),
+              |s| Ok(s.into()));
 }
